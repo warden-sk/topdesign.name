8909b2a33eb8323fac4d5603c6277064d23555c1
Marek Kobida <marek.kobida@gmail.com>
2022-12-03 13:56:07 +0100

    update

---
+++ private/messages.ts
@@ -0,0 +1,8 @@
+export const messages_sk = {
+  ADD_PRODUCT_TO_ORDER: 'Pridať produkt do objednávky',
+  DELETE_PRODUCT_FROM_ORDER: 'Odstrániť produkt z objednávky',
+  OPTIONS: 'Možnosti',
+  PRODUCT: 'Produkt',
+  TABLE: 'Tabuľka',
+  WITHOUT_VAT: 'bez DPH',
+};
