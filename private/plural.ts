c9aaf1a3413ec3dfd0f46edc280b6493a8135545
Marek Kobida <marek.kobida@gmail.com>
2022-12-20 15:55:53 +0100

    up

--- private/plural.ts
+++ private/plural.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 Marek Kobida
+ * Copyright 2023 Marek Kobida
  */
 
 function plural(a: number | string, b: string[]): string {
--- webpack.ts
+++ webpack.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 Marek Kobida
+ * Copyright 2023 Marek Kobida
  */
 
 import common from '@warden-sk/helpers/webpack/common';
